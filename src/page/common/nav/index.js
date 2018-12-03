bd8bb1838cea2124ecbaa1fcbdd131a0e9c79ff3
“tiandenghao <tiandh987@163.com>
2018-12-03 10:17:36 +0800

    购物车页面，逻辑

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,13 +1,13 @@
 require('./index.css');
 var _mm     = require('util/mm.js');
 var _user   = require('service/user-service.js');
-// var _cart   = require('service/cart-service.js');
+var _cart   = require('service/cart-service.js');
 // 导航
 var nav = {
     init : function(){
         this.bindEvent();
         this.loadUserInfo();
-        // this.loadCartCount();
+        this.loadCartCount();
         return this;
     },
     bindEvent : function(){
@@ -36,15 +36,15 @@ var nav = {
         }, function(err){
             // do nothing
         });
-    }
+    },
     // 加载购物车数量
-//     loadCartCount : function(){
-//         _cart.getCartCount(function(res){
-//             $('.nav .cart-count').text(res || 0);
-//         }, function(errMsg){
-//             $('.nav .cart-count').text(0);
-//         });
-//     }
+    loadCartCount : function(){
+        _cart.getCartCount(function(res){
+            $('.nav .cart-count').text(res.data || 0);
+        }, function(err){
+            $('.nav .cart-count').text(0);
+        });
+    }
 };
 
 module.exports = nav.init();
